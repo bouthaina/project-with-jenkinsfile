10a44a675ce958080c542244bec7ea07e7501b94
bouthaina <bouthainabakouch@gmail.com>
2025-05-20 12:10:30 +0100

    add dockerfile and dockercompose

--- ProductApp.API/Program.cs
+++ ProductApp.API/Program.cs
@@ -1,18 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using ProductApp.API.Data;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Récupérer la chaîne de connexion depuis la configuration ou les variables d'environnement
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Ajouter le contexte de base de données
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
+// Ajouter les contrôleurs
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer( );
-builder.Services.AddSwaggerGen();
 
-// Add CORS policy
+// Configurer Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProductApp API", Version = "v1" });
+});
+
+// Configurer CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
@@ -25,20 +34,31 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurer le pipeline de requêtes HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductApp API v1"));
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// Appliquer les migrations automatiquement au démarrage
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
 
-// Use CORS
+// Utiliser CORS avant les endpoints
 app.UseCors("AllowAll");
 
+app.UseRouting();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
--- ProductApp.Client/Program.cs
+++ ProductApp.Client/Program.cs
@@ -8,9 +8,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5144/") });
+//var apiUrl = builder.Configuration["API_URL"] ?? builder.HostEnvironment.BaseAddress;
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
+// Configuration du HttpClient avec l'URL de l'API
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 builder.Services.AddScoped<ProductService>();
 
 await builder.Build().RunAsync();
